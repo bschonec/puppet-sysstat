cc72aa8699b15c098d9ab8a9fef552dfef54abd1
root <root@chewrag7.healthplan.com>
2017-01-16 08:13:15 -0500

    Added paramaters for cron job so that we can control how often sa1 runs.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,48 @@
 # == Class: sysstat
 #
-# === Examples
+# Parameters
+# ----------
 #
-# include sysstat
+# * `interval`
+# The interval in seconds between runs of 'sa1'.  This should probably be set
+# to '1' and never anything else.  See: 'man sa1'
+# Default: 1
+#
+# * `count`
+# The number of times to run 'sa1'.  See: 'man sa1'
+# Default: 1
+#
+# * `minute`
+# The minute parameter of /etc/cron.d/sysstat.
+# Default:  "*/10"
+#
+# * `hour`
+# The hour parameter of /etc/cron.d/sysstat.
+# Default:  "*"
+#
+# * `day`
+# The day parameter of /etc/cron.d/sysstat.
+# Default:  "*"
+#
+# * `month`
+# The month parameter of /etc/cron.d/sysstat.
+# Default:  "*"
+#
+# * `day_of_week`
+# The day of the week parameter of /etc/cron.d/sysstat.
+# Default:  "*"
+#
+#
+# Examples
+# --------
+#
+# Run sar1 every minute:
+#
+# @example
+#    class { 'sysstat':
+#      minute => "*/1",
+#    }
+#
 #
 # === Authors
 #
@@ -12,7 +52,23 @@
 #
 # Copyright (C) 2013 Joshua Hoblitt
 #
-class sysstat inherits sysstat::params {
+class sysstat (
+  $interval    = $::sysstat::params::interval,
+  $count       = $::sysstat::params::count, 
+  $minute      = $::sysstat::params::minute,
+  $hour        = $::sysstat::params::hour, 
+  $day         = $::sysstat::params::day, 
+  $month       = $::sysstat::params::month, 
+  $day_of_week = $::sysstat::params::day_of_week,
+) inherits sysstat::params {
+
+  file {'/etc/cron.d/sysstat':
+    owner => 'root',
+    group => 'root',
+    mode  => '0600',
+    content => template('sysstat/sysstat.erb'),
+  }
+
   class { 'sysstat::install': } ->
   class { 'sysstat::service': } ->
   Class['Sysstat']
--- manifests/params.pp
+++ manifests/params.pp
@@ -11,6 +11,15 @@
 # Copyright (C) 2013 Joshua Hoblitt
 #
 class sysstat::params {
+
+  $interval    = "1"
+  $count       = "1"
+  $minute      = "*/10"
+  $hour        = "*"
+  $day         = "*"
+  $month       = "*"
+  $day_of_week = "*"
+
   case $::osfamily {
     'debian', 'redhat': {
       $sysstat_package = 'sysstat'
